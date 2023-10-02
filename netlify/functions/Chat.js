9bc1e64ed6d128caba817c3c9b6477878233872c
Darkskittlz (Linux) <darkskiiittles@gmail.com>
2023-10-01 23:52:43 -0400

    close to handling cors error

--- netlify/functions/Chat.js
+++ netlify/functions/Chat.js
@@ -1,11 +1,17 @@
-const express = require("express");
-const cors = require("cors");
-const axios = require("axios");
-const serverless = require("serverless-http");
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: true }));
+
+app.use(
+  cors({
+    origin: '*', // Allow requests from any origin (for development)
+    methods: ['GET', 'POST'], // Define the HTTP methods allowed
+    allowedHeaders: ['Content-Type', 'Authorization'], // Define the allowed headers
+  })
+);
 
 const authenticate = async (req, res) => {
   const { username } = req.body;
@@ -14,17 +20,17 @@ const authenticate = async (req, res) => {
     const r = await axios.put(
       'https://api.chatengine.io/users/',
       { username: username, secret: username, first_name: username },
-      { headers: { "private-key": "ff622dd3-fb29-499d-b30b-24adacfc2145" } }
+      { headers: { 'private-key': 'ff622dd3-fb29-499d-b30b-24adacfc2145' } }
     );
     return res.status(r.status).json(r.data);
   } catch (e) {
     return res.status(e.response.status).json(e.response.data);
   }
-  return res.json({ username: username, secret: "sha256..." });
+  return res.json({ username: username, secret: 'sha256...' });
 };
 
 // Define your route using the .post method
-app.post("/", authenticate);
+app.post('/', authenticate);
 
-// Export your app using serverless-http
-module.exports.handler = serverless(app);
+
+export { app as handler };
